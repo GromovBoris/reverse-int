1a2e03ad657a1947cb41287d895c6232e87b7871
GromovBoris <drunkgonzo@gmail.com>
2023-01-09 00:08:25 +0300

    reverse number: complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let answer = Math.abs(n);
+    answer = answer.toString().split("").reverse().join("");
+    // answer = Math.abs(answer);
+    // console.log(answer);
+    return answer;
+};
+
+// reverse(-1234);
